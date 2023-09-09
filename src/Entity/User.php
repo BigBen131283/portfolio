6f864a4c66b105cc0bf9f44bd56545676a99b643
BigBen83 <benjamin.toubhans@orange.fr>
2023-09-09 19:35:46 +0200

    still some bugs to fix

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,10 +3,13 @@
 namespace App\Entity;
 
 use App\Repository\UserRepository;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-class User
+class User implements PasswordAuthenticatedUserInterface, UserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
@@ -18,6 +21,9 @@ class User
 
     #[ORM\Column(length: 255)]
     private ?string $password = null;
+
+    #[ORM\Column(type: Types::ARRAY, nullable: true)]
+    private ?array $role = null;
 
     public function getId(): ?int
     {
@@ -46,5 +52,40 @@ class User
         $this->password = $password;
 
         return $this;
+    }
+
+    public function getRole(): ?array
+    {
+        return $this->role;
+    }
+
+    public function setRole(?array $role): static
+    {
+        $this->role = $role;
+
+        return $this;
+    }
+
+    public function getRoles(): array
+    {
+        return array('ROLE_ADMIN', 'TOTO');
+        // return $this->role;
+    }
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+        /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string 
+    {
+        return (string) $this->email;
     }
 }
