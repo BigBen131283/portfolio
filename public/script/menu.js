dbff5571b0e0b12fb51591af38010cc34a28a293
BigBen83 <benjamin.toubhans@orange.fr>
2023-09-09 17:31:55 +0200

    230909 - crud user and login

--- public/script/menu.js
+++ public/script/menu.js
@@ -2,6 +2,8 @@
 
 const dropdownLink = Array.from(document.querySelectorAll(".dropdown-link"));
 const dropdownContent = Array.from(document.querySelectorAll(".dropdown-content"));
+const dropdownContainer = Array.from(document.querySelectorAll(".dropdown-container"));
+const allLinks = document.querySelectorAll("div.dropdown-content > a");
 
 dropdownLink.forEach(link => {
     link.addEventListener('mouseenter', (e) => {
@@ -13,7 +15,7 @@ dropdownLink.forEach(link => {
     })
 })
 
-dropdownContent.forEach(content => {
+dropdownContainer.forEach(content => {
     content.addEventListener('mouseleave', (e)=> {
         for(let i = 0; i<dropdownContent.length; i++){
             dropdownContent[i].classList.remove('active');
