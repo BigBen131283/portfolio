fc3b2f2de9aa279b764a8ed6d5f0b51bbf80121e
BigBen83 <benjamin.toubhans@orange.fr>
2023-09-12 17:13:06 +0200

    230912 - entities (no relation)

---
+++ src/Entity/Skills.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SkillsRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SkillsRepository::class)]
+class Skills
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $language = null;
+
+    #[ORM\Column(type: Types::SMALLINT)]
+    private ?int $mastery = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $logo = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLanguage(): ?string
+    {
+        return $this->language;
+    }
+
+    public function setLanguage(string $language): static
+    {
+        $this->language = $language;
+
+        return $this;
+    }
+
+    public function getMastery(): ?int
+    {
+        return $this->mastery;
+    }
+
+    public function setMastery(int $mastery): static
+    {
+        $this->mastery = $mastery;
+
+        return $this;
+    }
+
+    public function getLogo(): ?string
+    {
+        return $this->logo;
+    }
+
+    public function setLogo(string $logo): static
+    {
+        $this->logo = $logo;
+
+        return $this;
+    }
+}
